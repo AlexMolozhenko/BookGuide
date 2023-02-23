c7dadedd064b1b5f46310bf2d364d62313968028
Alexsey Molozhenko <jukov20150@gmail.com>
2023-02-23 22:45:06 +0200

    create author and book controller

---
+++ views/author/index.php
@@ -0,0 +1,6 @@
+<?php
+/** @var yii\web\View $this */
+?>
+<h1>author/index</h1>
+
+<button class=" btn btn-primary" type="button"> add Author </button>
---
+++ views/book/index.php
@@ -0,0 +1,9 @@
+<?php
+/** @var yii\web\View $this */
+use yii\helpers\Url;
+use yii\helpers\Html;
+?>
+<h1>book/index</h1>
+
+<button class=" btn btn-primary" type="button"><?= Html::img(  Url::to('@web/icon/icon-plus.png'), ['alt' => 'plusico']) ?><?= Html::img(  Url::to('@web/icon/book.ico'), ['alt' => 'bookico']) ?></button>
+
